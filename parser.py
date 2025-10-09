e79e2c501d81b87526d5c1ada6fd0f4dd19696ae
Sakib678 <sakibarif@hotmail.co.uk>
2025-10-09 14:08:43 -0400

    Added parser which reads and prints json file

---
+++ parser.py
@@ -0,0 +1,10 @@
+import json
+
+with open('cowrie.json', 'r') as file:
+    for line in file:
+        try:
+            json_data = json.loads(line)
+            print(json_data)
+        except json.JSONDecodeError:
+            pass
+
