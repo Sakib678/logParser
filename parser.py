4c231227deb7ede6d4ca60ebf9001841fdb0b717
Sakib678 <sakibarif@hotmail.co.uk>
2025-10-10 13:00:05 -0400

    Added counter which counts the events and prints out the number

--- parser.py
+++ parser.py
@@ -4,7 +4,7 @@ with open('cowrie.json', 'r') as file:
     for line in file:
         try:
             json_data = json.loads(line)
-            print(json_data)
+            print(json_data['eventid'])
         except json.JSONDecodeError:
             pass
 
