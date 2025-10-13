e21a03121cc0bc787a6e05c4979927ecf3476a99
Sakib678 <sakibarif@hotmail.co.uk>
2025-10-13 13:36:26 -0400

    Makes the counter look more presentable

--- parser.py
+++ parser.py
@@ -1,10 +1,23 @@
+from collections import Counter
 import json
+
+event_counter = Counter() 
+success_counter = Counter()
+failure_counter = Counter()
+ip_counter = Counter()
+username_counter = Counter() 
 
 with open('cowrie.json', 'r') as file:
     for line in file:
         try:
             json_data = json.loads(line)
-            print(json_data['eventid'])
+            event_id = json_data.get('eventid', 'unknown')
+            event_counter[event_id] += 1
+
         except json.JSONDecodeError:
-            pass
+            continue
+
+print("----- Event Counter -----")
+for event, count in event_counter.most_common():
+    print(f"{event}: {count}")
 
